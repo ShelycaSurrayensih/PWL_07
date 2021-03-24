02ef6777cd0b533b397f5e152182bdf9ad76c788
Shelyca Surrayensih <70507995+ShelycaSurrayensih@users.noreply.github.com>
2021-03-24 07:16:32 +0700

    update

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -1,12 +1,12 @@
-@extends('mahasiswas.layout')
-
+@extends('mahasiswa.layout')
+ 
 @section('content')
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center">
         <div class="card" style="width: 24rem;">
             <div class="card-header">
                Detail Mahasiswa
-            </div>
+            </div> 
             <div class="card-body">
                 <ul class="list-group list-group-flush">
                 <li class="list-group-item"><b>Nim </b>{{$Mahasiswa->nim}}</li>
@@ -16,7 +16,7 @@
                 <li class="list-group-item"><b>No. Handphone </b>{{$Mahasiswa->no_handphone}}</li>
                 </ul>
             </div>
-        <a class="btn btn-success mt-3" href="{{ route('mahasiswas.index') }}">Kembali</a>
+        <a class="btn btn-success mt-3" href="{{ route('mahasiswa.index') }}">Kembali</a>
         </div>
     </div>
 </div>
